1506e97ec5866cc0baaa6d80789c5d583c55690b
marcus-wong-ics4u1-02-25 <marcus.wong5@student.tdsb.on.ca>
2025-04-07 01:07:53 +0000

    same assignment 3.1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,26 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		System.out.println(sumOfDigits(7777));
 
 	}
+	public static int sumOfDigits(int x) {
+		if (x < 0) {
+			x = -x;
+		}
+		if (x >= 0 && x <= 9) {
+			return x;
+		}
+		return sumOfDigits(x / 10) + (x % 10);
 
-	
-
+		}
+	public static int factorial(int n) {
+		if (n == 0) {
+			return 1;
+		}
+		if (n < 0) {
+			return 0;
+		}
+		return n * factorial(n - 1);
+		}
 }
